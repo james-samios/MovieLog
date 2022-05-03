1ed2b1590c7313de8870b9479856686425df36bd
James Samios <git@mail.samios.io>
2022-05-03 16:33:51 +1000

    Initial Commit

---
+++ MovieLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieLog
+//
+//  Created by James Samios on 3/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
