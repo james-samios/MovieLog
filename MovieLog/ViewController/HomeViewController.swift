fcef30cbbae6a9367d8a7746600f35a5b13a5d35
James Samios <git@mail.samios.io>
2022-05-16 19:19:56 +1000

    Change methods for the HomeViewController to link it to the API.

--- MovieLog/ViewController/HomeViewController.swift
+++ MovieLog/ViewController/HomeViewController.swift
@@ -8,27 +8,48 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet var latestMovies: UITableView!
+    @IBOutlet var recommendedMovies: UITableView!
+    
+    let latestMoviesSource = LatestMovieSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.latestMovies.dataSource = self.latestMoviesSource
     }
-    
 }
 
-extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+class LatestMovieSource: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as? HomeTableViewCell else {fatalError("Unable to create table")}
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "LatestMoviesTableViewCell", for: indexPath) as? LatestMoviesTableViewCell else {fatalError("Unable to create table")}
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 170
     }
+    
+}
+
+class LatestMovieCell: UICollectionViewCell {
+    
+    @IBOutlet var posterImg: UIImageView!
+    
+}
+
+class RecommendedMovieCell: UICollectionViewCell {
     
 }
---
+++ MovieLog/ViewController/LatestMoviesTableViewCell.swift
@@ -0,0 +1,82 @@
+//
+//  HomeTableViewCell.swift
+//  OtherMovie
+//
+//  Created by Alexandra Streeton on 11/5/2022.
+//
+
+import UIKit
+import Kingfisher
+
+class LatestMoviesTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    var movies: [Movie] = []
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        DBConnector.instance.getLatestMovies(callback: {
+            movies in
+            self.movies = movies
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        })
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "latestMovieCell", for: indexPath) as! LatestMovieCell
+        
+        
+        let posterView = cell.posterImg!
+        guard movies.indices.contains(indexPath.row) else {
+            
+            posterView.kf.indicatorType = .activity
+            cell.posterImg = posterView
+            return cell
+        }
+        let score = movies[indexPath.row]
+        
+        
+        // Load the image. This can also retrieve it from cache.
+        let poster = score.getPosterUrl()
+        if (poster.isEmpty) {
+            // unavailable image to be set here.
+            return cell
+        }
+        let url = URL(string: score.getPosterUrl())
+        let processor = DownsamplingImageProcessor(size: posterView.bounds.size)
+        posterView.kf.indicatorType = .activity
+        posterView.kf.setImage(
+            with: url,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+        
+        cell.posterImg = posterView
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 140, height: 160)
+    }
+}
+
